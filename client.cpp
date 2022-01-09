b0d4204dc1698184f1c14b908aaae85e9ce7d6db
BrettBass <bbass@ucsd.edu>
2022-01-08 18:51:16 -0800

    client side is now up

--- client.cpp
+++ client.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+
+//unix libraries
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -20,6 +22,58 @@
 #include <fcntl.h>
 #include <fstream>
 
+#define BUFFER_SIZE 1024
+
 int main(int __argc, char *__argv){
-    
+    //when accessing the server we want to use a specific port and ip
+    if(__argc != 3){
+        std::cout << "Usage: ./client <ip> <port>" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    //hold the port number and ip
+    int port = atoi(__argv[2]);
+    std::string ip = __argv[1];
+
+    //create a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd == -1){
+        std::cout << "Error creating socket" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    //connect to the server
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+    if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
+        std::cout << "Error connecting to server" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    //create a buffer to hold the message
+    char buffer[BUFFER_SIZE];
+
+    //create a file to hold the message
+    std::ofstream outfile;
+    outfile.open("message.txt");
+
+    //read the message from the server
+    int bytes_read = read(sockfd, buffer, BUFFER_SIZE);
+    if(bytes_read == -1){
+        std::cout << "Error reading from server" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    //write the message to the file
+    outfile << buffer;
+
+    //close the file
+    outfile.close();
+
+    //close the socket
+    close(sockfd);
+
+    return 0;
 }
